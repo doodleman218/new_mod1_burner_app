b93c9c54886a781768707c1424677cf4928e8ecd
doodleman218 <dcwolff218@gmail.com>
2020-04-08 15:10:20 -0500

    seeding the burner names

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,18 @@ Burner.destroy_all
 Event.destroy_all
 Camp.destroy_all
 
-binding.pry
 
-#member seed data
-david = Member.create(name: "David", candy: true)
+#burner seed data
+david = Burner.create(name: "David", candy: true)
+shadow = Burner.create(name: "Shadow", candy: true)
+cucumber = Burner.create(name: "Cucumber", candy: true)
+angel = Burner.create(name: "Angel", candy: true)
+tanzi = Burner.create(name: "Tanzi", candy: true)
+ted = Burner.create(name: "Ted", candy: true)
+alla = Burner.create(name: "Alla", candy: true)
+rith = Burner.create(name: "Rith", candy: false)
+face = Burner.create(name: "Face", candy: false)
+zee = Burner.create(name: "Zee", candy: false)
+paul = Burner.create(name: "Paul", candy: false)
+
+binding.pry
